36ea9e031ecd08f2674e23988091f752361810f9
KM Tu <tutu522@gmail.com>
2015-10-25 00:58:40 +0900

    Reduces the job time for ermod-ntf2 to 4 hours (~2 hours are needed)

--- ermod-ntf2/soln/run_ermod.sh
+++ ermod-ntf2/soln/run_ermod.sh
@@ -2,7 +2,7 @@
 #PJM -g gh24
 #PJM -L "rscgrp=short"
 #PJM -L "node=72"
-#PJM -L "elapse=06:00:00"
+#PJM -L "elapse=04:00:00"
 #PJM --mpi "proc=1152"
 #PJM -m e
 #PJM --mail-list kmtu@esicb.kyoto-u.ac.jp
