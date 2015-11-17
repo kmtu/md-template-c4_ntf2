476204e98dd1715d8a57ac12820903c3f0cf7aac
KM Tu <tutu522@gmail.com>
2015-11-17 19:38:22 +0900

    Changes run_md.sh to kei format

--- run_md.sh
+++ run_md.sh
@@ -1,26 +1,46 @@
 #!/bin/sh
-#PJM -g gh24
-#PJM -L "rscgrp=short"
-#PJM -L "node=8"
-#PJM -L "elapse=02:00:00"
-#PJM --mpi "proc=32"
+#PJM --rsc-list "node=16"
+#PJM --rsc-list "rscgrp=small"
+#PJM --rsc-list "elapse=03:00:00"
+#PJM --name md-nve
+#PJM --mpi "shape=16"
+#PJM --mpi "proc=128"
+#PJM -s
+#
+#PJM --stg-transfiles all
+#
+#PJM --vset gromacs_path=/volume2/home/hp150268/k02368/opt/gromacs-5.1.1/
+#PJM --vset mdpath=/volume41/data/hp150268/k02368/kmtu/C4mimNTf2-Ludwig-216-2015/3-nve/md99
+#PJM --vset top=C4mimNTf2.top
+#PJM --vset prev=md-npt
+#PJM --vset stage=md-nve
+#
+#PJM --stgin "${gromacs_path}/bin/gmx_d ./"
+#PJM --stgin "${gromacs_path}/bin/mdrun_mpi_d ./"
+#PJM --stgin "${gromacs_path}/gmxtop.tgz ./"
+#PJM --stgin "${mdpath}/../../${top} ./"
+#PJM --stgin "${mdpath}/../${stage}.mdp ./"
+#PJM --stgin "${mdpath}/${prev}.tpr ./"
+#PJM --stgin "${mdpath}/${prev}.cpt ./"
+#
+#PJM --stgout "mdout.mdp ./"
+#PJM --stgout "${stage}.* ./"
 #PJM -m e
 #PJM --mail-list kmtu@esicb.kyoto-u.ac.jp
 
-GMDIR=/home/f24005/local/gromacs-5.1/bin
-GMX=$GMDIR/gmx_d
-GROMPP="$GMX grompp"
-TPBCONV="$GMX convert-tpr"
-MDRUN=$GMDIR/mdrun_mpi_d
+. /work/system/Env_base
+export GMXLIB="./share/gromacs/top"
+grompp="./gmx_d grompp"
+mdrun="./mdrun_mpi_d"
 
-TOP=../../C4mimNTf2
-PREV=md-npt
-STAGE=md-nve
+tar -zxvf ./gmxtop.tgz
 
-MAXH=2
-NPME=4
+top=C4mimNTf2.top
+prev=md-npt
+stage=md-nve
 
+maxh=3
+npme=-1
 
-$GROMPP -f ../${STAGE}.mdp -c $PREV.tpr -t $PREV.cpt -p $TOP.top -o $STAGE.tpr 
-mpiexec $MDRUN -s $STAGE.tpr -deffnm $STAGE -npme $NPME -maxh $MAXH #-cpi $STAGE.cpt
-
+$grompp -f $stage.mdp -c $prev.tpr -t $prev.cpt -p $top -o $stage.tpr
+mpiexec $mdrun -deffnm $stage -npme $npme -maxh $maxh #-cpi $stage.cpt
